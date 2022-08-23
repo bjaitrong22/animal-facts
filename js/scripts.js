b86fef23f5135febfbfd23e7dc4f4b3ad91c397f
Bai Jaitrong <bjaitrong@comcast.net>
2022-08-22 22:58:43 -0600

    add dolphins options and you didn't choose from our list message.

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,28 @@ window.onload = function() {
   form.onsubmit = function(event) {
     event.preventDefault();
   
-
-    let turtle = document.getElementById("turtle");
+    let turtle = document.getElementById("turtles");
     turtle.setAttribute("class", "hidden");
-    let whale = document.getElementById("whale");
+    
+    let whale = document.getElementById("whales");
     whale.setAttribute("class", "hidden");
 
+    let dolphin = document.getElementById("dolphins");
+    dolphin.setAttribute("class", "hidden");
+
+    let didNotChoose = document.getElementById("didNotChoose");
+    didNotChoose.setAttribute("class", "hidden");
 
     const animal = document.querySelector("input#animal").value;
 
-    if (animal === "turtle") {
+    if (animal === "turtles") {
       turtle.removeAttribute("class");
-    } else {
+    } else if (animal === "whales") {
       whale.removeAttribute("class");
+    } else if (animal === "dolphins") {
+      dolphin.removeAttribute("class");
+    } else {
+      didNotChoose.removeAttribute("class");
     }
   };
 };
